50099a21213d7306a51cac928244b0102ce5931b
raphaelpg <raphael_pinto@outlook.com>
2019-12-18 22:47:51 +0100

    update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+let SimpleStorage = artifacts.require("./SimpleStorage.sol");
+let TokenAly = artifacts.require("./TokenAly.sol");
+let SwapAly = artifacts.require("./SwapAly.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(TokenAly);
+  deployer.deploy(SwapAly, TokenAly.address);
 };
