b9095161e672449f4fcc47c2b1bbf82a61aacdb0
raphaelpg <raphael_pinto@outlook.com>
2020-01-20 16:22:53 +0100

    adding natSpecs

--- contracts/TokenERC20Dai.sol
+++ contracts/TokenERC20Dai.sol
@@ -2,6 +2,10 @@ pragma solidity 0.5.12;
 
 import "./ERC20.sol";
 
+/// @title ALY ERC-20 contract
+/// @author Raphael Pinto Gregorio
+/// @notice Basic ERC-20 token based on OpenZepellin ERC-20 standard
+/// @dev The token has all classic ERC-20 functions: totalSupply(), balanceOf(), transfer(), allowance(), approve(), transferFrom(). It has two decimals.
 contract TokenERC20Dai is ERC20 {
     string public name;
     string public symbol;
@@ -18,6 +22,10 @@ contract TokenERC20Dai is ERC20 {
         _mint(msg.sender, 1000000);
     }
 
+    /// @author Raphael Pinto Gregorio
+    /// @notice return contract owner
+    /// @dev basic function
+    /// @return contract owner address
     function getOwner() external view returns(address) {
         return _owner;
     }
